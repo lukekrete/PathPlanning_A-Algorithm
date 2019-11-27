8218060638170e6290aede84230899332073d237
lukekrete <lukekrete@gmail.com>
2019-11-27 13:50:34 -0500

    Update AI_TP.py

--- AI_TP.py
+++ AI_TP.py
@@ -2,12 +2,51 @@ import string
 import math
 import sys
 import os
+from queue import PriorityQueue
 
 def main():
     filename = 'input.txt'
-    rL,rW,numRobots,robotOneX,robotOneY,robotTwoX,robotTwoY,rendX,rendY,room = getInput(filename)
+    rL,rW,numRobots,robotArray,rend,room = getInput(filename)
     #printRoom(room,rL,rW)
+    #print(type(robotArray[0]))
+    for robot in robotArray:
+        path = Astar(room,robot,rend,rL,rW)
 
+def Astar(room,start, goal, rL, rW):
+    frontier = PriorityQueue()
+    frontier.put(start,0)
+    previous = {}
+    current_cost = {}
+    previous[start] = None
+    current_cost[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+        if current == goal:
+            break
+        neighbors = getNeighbors(current,rL,rW)
+        for element in neighbors:
+            new_cost = current_cost[current] + cost(current,element)
+            if element not in current_cost or new_cost < current_cost[element]:
+                current_cost[element] = new_cost
+                priority = new_cost + cost(goal,element)
+                frontier.put(element, priority)
+                previous[element] = current
+                
+
+def cost(current, nextNode):
+    cost1 = (current[0]**2 - nextNode[0]**2)
+    cost2 = (current[1]**2 - nextNode[0]**2)
+    final_cost = (cost1 + cost2)**0.5
+    return final_cost
+
+    
+def getNeighbors(current):
+    neighbors = []
+    if current[0] > 0 and current[1] > 0 and current[1] < rL and current[0] < rW:
+        
+        
+    
 def printRoom(room,rL,rW):
     for i in range(rL):
         print()
@@ -17,33 +56,30 @@ def printRoom(room,rL,rW):
             
 def getInput(filename):
     with open(filename) as fp:
-        count = 1
-        count2 = 0
-        for line in fp:
-            #print(count)
-            if count == 1:
-                words = line.split()
-                rL = int(words[0])
-                rW = int(words[1])
-                room = [[0]*rW]*rL
-            elif count == 2:
-                words = line.split()
-                numRobots = int(words[0])
-            elif count == 3:
-                words = line.split()
-                robotOneX = int(words[0])
-                robotOneY = int(words[1])
-            elif count == 4:
-                words = line.split()
-                robotTwoX = int(words[0])
-                robotTwoY = int(words[1])
-            elif count == 5:
-                words = line.split()
-                rendX = int(words[0])
-                rendY = int(words[1])
-            else:
-                row = line.split()
-                room[count2] = row
-                count2+=1
-            count += 1
-    return rL,rW,numRobots,robotOneX,robotOneY,robotTwoX,robotTwoY,rendX,rendY,room
+        line = fp.readline()
+        words = line.split()
+        rL = int(words[0])
+        rW = int(words[1])
+        room = [[0]*rW]*rL
+
+        line=fp.readline()
+        words = line.split()
+        numRobots = int(words[0])
+
+        robotArray = []
+        for i in range(numRobots):
+            line = fp.readline()
+            words = line.split()
+            robotArray.append((int(words[0]),int(words[1])))
+
+        line = fp.readline()
+        words = line.split()
+        rend = ((int(words[0]),int(words[1])))
+        #rendX = words[0]
+        #rendY = words[1]
+        for i in range(rL):
+            line=fp.readline()
+            row = line.split()
+            room[i] = row
+          
+    return rL,rW,numRobots,robotArray,rend,room
